584ed9ae9ad4ddc4362ec2cc3df0344ba2f6d2fe
Nick Evans <nickevans@ufl.edu>
2015-04-21 23:38:58 -0400

    Added initial fragment and layout files

---
+++ app/src/main/java/com/example/nick/hsvcolor/HueListFragment.java
@@ -0,0 +1,10 @@
+package com.example.nick.hsvcolor;
+
+import android.app.Fragment;
+
+/**
+ * Created by Nick on 4/21/2015.
+ */
+public class HueListFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/example/nick/hsvcolor/SaturationListFragment.java
@@ -0,0 +1,9 @@
+package com.example.nick.hsvcolor;
+
+import android.app.Fragment;
+
+/**
+ * Created by Nick on 4/21/2015.
+ */
+public class SaturationListFragment extends Fragment {
+}
